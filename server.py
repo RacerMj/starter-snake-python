aa18c0dc4b17fc863f171acad8f8719047d2b6af
RacerMj <racer.mj@gmail.com>
2020-04-22 18:36:50 -0400

    hunt for food update - v1

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import random
 
 import cherrypy
 import json
+from _socket import IPPROTO_ICLFXBM
 
 
 """
@@ -59,81 +60,128 @@ class Battlesnake(object):
         
         goodMove = False
 
-        triedMoves = []
-        possible_moves = ["right", "up", "down", "left"]
-        while goodMove == False and len(triedMoves) < 4:
+        # Choose a direction to move in
+        # random
+        #while goodMove == False:
+        #    move = random.choice(possible_moves)
+        #    try:
+        #        # see if we've tried it already
+        #        moveIndex = triedMoves.index(move)
+        #    except ValueError:
+        #        # we got an error, indicating it wasn't in our triedMoves list
+        #        goodMove = True
+
+        # find closest food
+        # get foodstuffs
+        foodstuffs = board["food"]
+        # set distance to farthest possible on board
+        distanceToFood = width
+        targetX = 0
+        targetY = 0
+        # calculate the distance to each piece of food
+        for f in foodstuffs:
+            fx = f["x"]
+            fy = f["y"]
+            distance = abs(headX-fx)+abs(headY-fy)
             
-            if len(triedMoves) < 4: 
-                # Choose a direction to move in
-                while goodMove == False:
-                    move = random.choice(possible_moves)
-                    try:
-                        # see if we've tried it already
-                        moveIndex = triedMoves.index(move)
-                    except ValueError:
-                        # we got an error, indicating it wasn't in our triedMoves list
-                        goodMove = True
+            # if this piece is closer, make it the new target
+            if distance < distanceToFood:
+                distanceToFood = distance
+                targetX = fx
+                targetY = fy
 
-                # Now adjust the target by the direction we're moving in             
-                if move == "up":
-                    targetX = headX
-                    targetY = headY - 1
-                    
-                elif move == "down":
-                    targetX = headX
-                    targetY = headY + 1
-                    
-                elif move == "left":
-                    targetY = headY
-                    targetX = headX - 1
-                    
-                else:
-                    targetY = headY
-                    targetX = headX + 1
-                    
-                # If the target is out of bounds, this is not a good move
-                if targetX < 0 or targetY < 0 or targetX >= width or targetY >= height:
-                    goodMove = False
-                    # Add the out of bounds move to the tried moves list
-                    triedMoves.append(move)
-                else:
-                    goodMove = True
+        # moves to try in order
+        moveXList = []
+        moveYList = []
+        moveList = ["", "", "", ""]
+                                
+        # what direction should we try first
+        if headX > targetX:
+            moveXList = ["left", "right"]
+        else:
+            moveXList = ["right", "left"]
+
+        # y is largest difference
+        if headY > targetY:
+            moveYList = ["up", "down"]
+        else:
+            moveYList = ["down", "up"]
+        
+        # try to move in direction of largest difference
+        if abs(headX-targetX) > abs(headY-targetY):
+            # X is largest difference
+            moveList = [moveXList[0], moveYList[0], moveXList[1], moveYList[1]]
+        else:
+            # y is largest difference
+            moveList = [moveYList[0], moveXList[0], moveYList[1], moveXList[1]]
+        
+        # start with the first move
+        currentMove = 0
+        
+        while goodMove == False and currentMove < 4:                
+            move = moveList[currentMove]
+            goodMove = True
+            
+            # Now adjust the target by the direction we're moving in             
+            if move == "up":
+                targetX = headX
+                targetY = headY - 1
                 
-                # If the move is bad, go back to start to try another
-                if goodMove == False:
-                    # Skip the rest of the while loop and start at the top again
-                    continue
-
-                # Get the snakes data from the parsed data
-                snakes = board["snakes"]
-                for s in snakes:
-                    body = s["body"]
-                    for b in body:
-                        if b["x"] == targetX and b["y"] == targetY:
-                            goodMove = False
-                            triedMoves.append(move)
-                            break
+            elif move == "down":
+                targetX = headX
+                targetY = headY + 1
+                
+            elif move == "left":
+                targetY = headY
+                targetX = headX - 1
+                
+            else:
+                targetY = headY
+                targetX = headX + 1
                         
-                    if goodMove == False:
-                        break
-
-                # If the move is bad, go back to start to try another
-                if goodMove == False:
-                    # Skip the rest of the while loop and start at the top again
-                    continue
-
-                # Get my own data from the parsed data
-                snakes = data["you"]
+            # If the target is out of bounds, this is not a good move
+            if targetX < 0 or targetY < 0 or targetX >= width or targetY >= height:
+                goodMove = False
+                    
+            # If the move is bad, go back to start to try another
+            if goodMove == False:
+                # increment the move to try
+                currentMove = currentMove + 1
+                # Skip the rest of the while loop and start at the top again
+                continue
+    
+            # Get the snakes data from the parsed data
+            snakes = board["snakes"]
+            for s in snakes:
                 body = s["body"]
                 for b in body:
                     if b["x"] == targetX and b["y"] == targetY:
                         goodMove = False
-                        triedMoves.append(move)
                         break
-            else:
-                # no moves left, return whatever we have and die
-                break
+                    
+                if goodMove == False:
+                    break
+    
+            # If the move hits another snake segment, this is not a good move
+            if goodMove == False:
+                # increment the move to try
+                currentMove = currentMove + 1
+                # Skip the rest of the while loop and start at the top again
+                continue
+    
+            # Get my own data from the parsed data
+            snakes = data["you"]
+            body = s["body"]
+            for b in body:
+                if b["x"] == targetX and b["y"] == targetY:
+                    goodMove = False
+                    break
                         
+            # If the move hits myself, this is not a good move
+            if goodMove == False:
+                # increment the move to try
+                currentMove = currentMove + 1
+
         print(f"move: {move}")
         return {"move":move}
 
